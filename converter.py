b1a6f44f43d0a4dd111bb00843a468d0e843cc69
Travis Bumgarner <travis.bumgarner@gmail.com>
2017-07-01 14:07:48 -0400

    Succesful Conversion of image to calculate brightness per area

--- converter.py
+++ converter.py
@@ -1,61 +1,62 @@
+# README
+# Start with Square Images Only
+
+
 import os
 
 from PIL import Image
 
 # User Defined Constants
-OUTPUT_WIDTH = 100  # PX
-OUTPUT_HEIGHT = 100  # PX
-SAMPLE_AREA = (10,10) # w, h of number of pixels that will make one circle
+OUTPUT_LENGTH = 200  # PX
+SAMPLE_LENGTH = 5
+
 CIRCLE_SIZES = [0, 1, 3, 5]  # PX
 
 # Other Constants
-CIRCLE_CENTER = (SAMPLE_AREA[0]/2, SAMPLE_AREA[1]/2)
-H_SAMPLES = int(OUTPUT_WIDTH/SAMPLE_AREA[0])  # Number of samples to make horizontally, used for generating output circle grid.
-V_SAMPLES = int(OUTPUT_HEIGHT/SAMPLE_AREA[1])  # See previous line
-OUTPUT_CIRCLE_GRID = [[0 for col in range(V_SAMPLES)] for row in range(H_SAMPLES)]
+CIRCLE_CENTER = (SAMPLE_LENGTH/2, SAMPLE_LENGTH/2)
+NUMBER_OF_SAMPLES = int(OUTPUT_LENGTH/SAMPLE_LENGTH)  # Number of samples to make in one direction, used for generating output circle grid.
+OUTPUT_CIRCLE_GRID = [[0 for col in range(NUMBER_OF_SAMPLES)] for row in range(NUMBER_OF_SAMPLES)]
+
+def sample_area(img, x_start, y_start, length):
+    total = 0
+    for y in range(y_start, y_start + length):
+        for x in range(x_start, x_start + length):
+            if img.mode == "L":  # “L” 8-bit greyscale. 0 means black, 255 means white.
+                pixel_grayscale = img.getpixel((x, y))
+            else:
+                pixel_rgb_sum = 0
+                for each_pixel in img.getpixel((x, y)): # take average of R, G, B
+                    pixel_rgb_sum += each_pixel
+                pixel_grayscale = pixel_rgb_sum / 3
+            total += pixel_grayscale
+    average = round(total / length ** 2)
+    return average
+
 
 def convert_image(img_location):
     with Image.open(img_location) as img:
-        INPUT_WIDTH, INPUT_HEIGHT = img.size
+        INPUT_LENGTH, _ = img.size
 
-        if INPUT_WIDTH > 100 or INPUT_HEIGHT > 100:
-            img = img.resize((OUTPUT_WIDTH, OUTPUT_HEIGHT))
+        # Resize Image if one side is > OUTPUT_LENGTH
+        if INPUT_LENGTH > OUTPUT_LENGTH:
+            img = img.resize((OUTPUT_LENGTH, OUTPUT_LENGTH))
 
-        for y in range(0, OUTPUT_HEIGHT):
-            for x in range(0, OUTPUT_WIDTH): # Todo figure out proper number of range to not go out of range
-                x_min = x * SAMPLE_AREA[0]
-                x_max = x * SAMPLE_AREA[0] + SAMPLE_AREA[0]
-                y_min = y * SAMPLE_AREA[1]
-                y_max = y * SAMPLE_AREA[1] + SAMPLE_AREA[1]
+        x_idx = 0
+        for x in range(0, OUTPUT_LENGTH, SAMPLE_LENGTH):
+            y_idx = 0
+            for y in range(0, OUTPUT_LENGTH, SAMPLE_LENGTH):
+                OUTPUT_CIRCLE_GRID[y_idx][x_idx] = sample_area(img, x, y, SAMPLE_LENGTH)
+                y_idx += 1
+            x_idx += 1
 
-                total = 0
-                for i in range(x_min, x_max):
-                    for j in range(y_min, y_max):
-                        if img.mode == "L": # “L” 8-bit greyscale. 0 means black, 255 means white.
-                            pixel_grayscale = img.getpixel((i, j))
-
-                        else:
-                            pixel_rgb_sum = 0
-                            for each_pixel in img.getpixel((i, j)):
-                                pixel_rgb_sum += each_pixel
-                            pixel_grayscale = pixel_rgb_sum  / 3
-                        total += pixel_grayscale
-                average = round(total / ((x_max - x_min) * (y_max - y_min)))
-                print(average)
-                OUTPUT_CIRCLE_GRID[y][x] = average
 
         pixels_out = []
-        image_out = Image.new("L", (H_SAMPLES, V_SAMPLES)) # “L” 8-bit greyscale. 0 means black, 255 means white.
+        image_out = Image.new("L", (NUMBER_OF_SAMPLES, NUMBER_OF_SAMPLES)) # “L” 8-bit greyscale. 0 means black, 255 means white.
         for row in OUTPUT_CIRCLE_GRID:
             for cell in row:
                 pixels_out.append(cell)
         image_out.putdata(pixels_out)
-        image_out.save('test_out3.png')
-
-
-
-
-
+        image_out.save('test_out4.png')
 
         filepath, filename = os.path.split(img_location)
         name, extension = filename.split('.')
@@ -64,4 +65,4 @@ def convert_image(img_location):
 
 
 if __name__ == '__main__':
-    convert_image('./test.jpg')
+    convert_image('./test2.jpg')
