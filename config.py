797280ef0c6bac4cb03b1f59bd8560e3bcd7cf12
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-05-22 18:23:44 -0400

    Moved things to config, removed some comments

---
+++ config.py
@@ -0,0 +1,10 @@
+# Currently the only thing that matters here are the ratios between values. Can't find inches in the dxf library
+UNITS = 'in'
+WIDTH = 4
+HEIGHT = 4
+SPACING_BETWEEN_CIRCLE_CENTERS = 0.15
+MAX_DIAMETER = SPACING_BETWEEN_CIRCLE_CENTERS * 0.75
+MIN_DIAMETER = 0.002
+SAMPLE_DATA = []
+IMAGE_FILE = './src/chs.jpg'
+DXF_FILE = './output/foo.dxf'
--- image_utils.py
+++ image_utils.py
@@ -1,17 +1,11 @@
 from PIL import Image
 from dxfwrite import DXFEngine as dxf
 
-# https://stackoverflow.com/questions/1969240/mapping-a-range-of-values-to-another
-
+from config import *
 
 
 class RasterImage():
-    """"
-    Description: Foo
-    """
-
     def __init__(self, img_src):
-        """Creates a new RasterImage"""
         try:
             self.img = Image.open(img_src).convert('L') # Convert to grayscale
         except IOError:
@@ -19,30 +13,29 @@ class RasterImage():
             return
 
         self.input_width, self.input_height = self.img.size
-        #
-        # self.output_units = ''
-        # while self.output_units not in ['in', 'cm']:
-        #     self.output_units = raw_input('[>] Select project units(in or cm): ')
-        #
-        # self.output_width = float(raw_input('[>] Output width: '))
-        # self.output_height = float(raw_input('[>] Output height: '))
-        #
-        # if(self.input_width / self.output_width - self.input_height / self.output_height > 0.1):
-        #     print('[!] The ratio of size from input image to output image is quite different. Consider editing first')
-        #
-        # self.output_spacing = float(raw_input('[>] Output density (circles per {}: ').format(self.output_units))
-        # spacing = float(raw_input('[>] Spacing between circles:'))
-        # self.output_max_diameter = 1 / self.output_spacing - spacing / 2
-        #
-        self.output_units = 'in'
-        self.output_width = 4
-        self.output_height = 4
-        self.output_spacing = 0.15
-        self.output_max_diameter = self.output_spacing * 0.9 # max diameter should be slightly smaller than spacing between circles
-        self.output_min_diameter = 0.002 # 1/32"
-        self.sample_data = []
 
-        self.dxf_drawing = dxf.drawing('/Users/tbumgarner/Documents/Programming/raster_cutter/foo.dxf')
+        # Currently the only thing that matters here are the ratios between values. Can't find inches in the dxf library
+        self.output_units = UNITS
+        self.output_width = WIDTH
+        self.output_height = HEIGHT
+        self.output_spacing = SPACING_BETWEEN_CIRCLE_CENTERS
+        self.output_max_diameter = MAX_DIAMETER
+        self.output_min_diameter = MIN_DIAMETER
+        self.sample_data = []
+        self.dxf_drawing = dxf.drawing(DXF_FILE)
+
+        if self.input_width / self.output_width - self.input_height / self.output_height > 0.1:
+            print('[!] The ratio of size from input image to output image is quite different. Consider editing first')
+
+        if SPACING_BETWEEN_CIRCLE_CENTERS < MAX_DIAMETER:
+            print('[!] The SPACING_BETWEEN_CIRCLE_CENTERS should be greater than the MAX_DIAMETER')
+
+        x_circles = int(self.output_width / self.output_spacing)
+        y_circles = int(self.output_height / self.output_spacing)
+        total_circles = int(x_circles * y_circles)
+        response = raw_input('X Circles: {}\nY Circles: {}\nTotal Circles: {}\nContinue? [y/n]'.format(x_circles, y_circles, total_circles))
+        if response != 'y' or 'Y':
+            return
 
     def luminance_to_inches(self, value):
         # 255 - value inverts the value so that the largest circles are for the darkest places.
@@ -54,6 +47,7 @@ class RasterImage():
         return inches
 
     def sample_image(self):
+        print('Sampling Image...')
         self.sample_data = []
 
         width_samples = self.output_width / self.output_spacing
@@ -65,23 +59,19 @@ class RasterImage():
         x = 0
         y = 0
 
-        # Swap x and y
-        # while x + pixels_per_sample_width <= self.input_width:
         while y + pixels_per_sample_height <= self.input_height:
             sample_row_data = []
-            # while y + pixels_per_sample_height <= self.input_height:
             while x + pixels_per_sample_width <= self.input_width:
                 luminance = self.sample_image_area(x, pixels_per_sample_width, y, pixels_per_sample_height)
                 sample_row_data.append(luminance)
-                # y += pixels_per_sample_height
                 x += pixels_per_sample_width
-            # y = 0
             x = 0
             self.sample_data.append(sample_row_data)
-            # x += pixels_per_sample_width
             y += pixels_per_sample_height
+        print('Sampling Complete.')
 
     def samples_to_dxf(self):
+        print('Converting samples to dxf...')
         # Start at the center of the first square
         x = self.output_spacing / 2
         y = self.output_spacing / 2
@@ -92,6 +82,7 @@ class RasterImage():
                 y += self.output_spacing
             y = self.output_spacing / 2
             x += self.output_spacing
+        print('Converting Complete.')
 
     def sample_image_area(self, x_start, x_delta, y_start, y_delta):
         luminance_total = 0
@@ -101,7 +92,6 @@ class RasterImage():
 
         sample_area = x_delta * y_delta
         luminance_average = luminance_total / sample_area
-        # print("sampling - ({}, {}) -> {}".format(x_start, y_start, luminance_average))
         return luminance_average
 
     def draw_borders(self):
@@ -118,10 +108,10 @@ class RasterImage():
     def save_dxf(self):
         self.dxf_drawing.save()
 
+
 if __name__ == '__main__':
-    r = RasterImage('./src/chs.jpg')
+    r = RasterImage(IMAGE_FILE)
     r.sample_image()
-    print('image sampled')
     r.samples_to_dxf()
     r.draw_borders()
     r.save_dxf()
