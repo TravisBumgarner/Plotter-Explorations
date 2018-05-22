82785463a975767013c37ca664a381bee72e20c8
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-05-22 16:39:42 -0400

    woo progress. Rewrote and refactored code time to cleanup some issues

--- image_utils.py
+++ image_utils.py
@@ -1,6 +1,9 @@
 from PIL import Image
 from dxfwrite import DXFEngine as dxf
 
+# https://stackoverflow.com/questions/1969240/mapping-a-range-of-values-to-another
+
+
 
 class RasterImage():
     """"
@@ -27,51 +30,60 @@ class RasterImage():
         # if(self.input_width / self.output_width - self.input_height / self.output_height > 0.1):
         #     print('[!] The ratio of size from input image to output image is quite different. Consider editing first')
         #
-        # self.output_density = float(raw_input('[>] Output density (circles per {}: ').format(self.output_units))
+        # self.output_spacing = float(raw_input('[>] Output density (circles per {}: ').format(self.output_units))
         # spacing = float(raw_input('[>] Spacing between circles:'))
-        # self.output_max_diameter = 1 / self.output_density - spacing / 2
+        # self.output_max_diameter = 1 / self.output_spacing - spacing / 2
         #
         self.output_units = 'in'
-        self.output_width = 10
-        self.output_height = 10
-        self.output_density = 0.1
-        self.output_max_diameter = 0.9
-
-        self.test_img = Image.new('RGB', (5000, 5000))
+        self.output_width = 2
+        self.output_height = 2
+        self.output_spacing = 0.25 # 1/4"
+        self.output_max_diameter = 0.25 - 0.0625 # 1/4" minus 1/16" gap
+        self.output_min_diameter = 0.003125 # 1/32"
+        self.sample_data = []
 
         self.dxf_drawing = dxf.drawing('/Users/tbumgarner/Documents/Programming/raster_cutter/foo.dxf')
 
+    def luminance_to_inches(self, value):
+        inches = self.output_min_diameter + (float(value) / float(255) * (self.output_max_diameter - self.output_min_diameter))
+        return inches
+
     def sample_image(self):
-        """
-        Sample entire area of image based on inputs.
-        """
+        self.sample_data = []
 
-        x_sample_count = self.output_width / self.output_density
-        y_sample_count = self.output_height / self.output_density
+        width_samples = self.output_width / self.output_spacing
+        height_samples = self.output_height / self.output_spacing
 
-        # pps = pixels per sample
-        x_pps = int(round(self.input_width / x_sample_count))
-        y_pps = int(round(self.input_height / y_sample_count))
+        pixels_per_sample_width = int(round(self.input_width / width_samples))
+        pixels_per_sample_height = int(round(self.input_height / height_samples))
 
-        x_start = 0
-        y_start = 0
+        x = 0
+        y = 0
 
-        test_img_x = 0
-        test_img_y = 0
+        # Swap x and y
+        while x + pixels_per_sample_width <= self.input_width:
+            sample_column_data = []
+            while y + pixels_per_sample_height <= self.input_height:
+                luminance = self.sample_image_area(x, pixels_per_sample_width, y, pixels_per_sample_height)
+                sample_column_data.append(luminance)
+                y += pixels_per_sample_height
+            y = 0
+            self.sample_data.append(sample_column_data)
+            x += pixels_per_sample_width
+        print(self.sample_data)
 
-        while x_start + x_pps <= self.input_width:
-            while y_start + y_pps <= self.input_height:
-                luminance = self.sample_image_area(x_start, x_pps, y_start, y_pps)
-                y_start += y_pps
-                self.test_img.putpixel((test_img_x, test_img_y), (luminance, luminance, luminance))
-                self.dxf_drawing.add(dxf.circle(radius=luminance / 2, center=(test_img_x * 255, test_img_y * 255)))
-                test_img_y += 1
-            y_start = 0
-            x_start += x_pps
-            test_img_y = 0
-            test_img_x += 1
+    def samples_to_dxf(self):
+        x = 0
+        y = 0
 
-    def sample_image_area(self, x_start, x_delta, y_start, y_delta, sample_method='luminance'):
+        for row in self.sample_data:
+            for cell in row:
+                self.dxf_drawing.add(dxf.circle(radius=self.luminance_to_inches(cell), center=(x, y)))
+                y += self.output_spacing
+            y = 0
+            x += self.output_spacing
+
+    def sample_image_area(self, x_start, x_delta, y_start, y_delta):
         luminance_total = 0
         for x in range(x_start, x_start + x_delta):
             for y in range(y_start, y_start + y_delta):
@@ -82,15 +94,12 @@ class RasterImage():
         # print("sampling - ({}, {}) -> {}".format(x_start, y_start, luminance_average))
         return luminance_average
 
-    def save_test_image(self, filename):
-        self.test_img.save(filename)
-
-    def save_dxf(self, filename):
+    def save_dxf(self):
         self.dxf_drawing.save()
-
 
 if __name__ == '__main__':
     r = RasterImage('./src/img.jpg')
     r.sample_image()
-    r.save_dxf('/Users/tbumgarner/Documents/Programming/raster_cutter/foo.dxf')
+    r.samples_to_dxf()
+    r.save_dxf()
 
