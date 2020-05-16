31cd42bc3f7c2efe50f55bf58d2d9c24f13d15fe
Travis Bumgarner <travis.bumgarner@gmail.com>
2020-05-16 14:03:56 -0400

    finished prototype

--- unified_parts_demo/unified_parts_demo.ino
+++ unified_parts_demo/unified_parts_demo.ino
@@ -14,7 +14,13 @@ int motorPosition = 0;
 #define TFT_DC         8
 
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+/* End Screen Setup */
 
+/* Motor Setup */
+#include <Stepper.h>
+const int stepsPerRevolution = 200; 
+Stepper myStepper(stepsPerRevolution, 3,4,5,6);
+/* End Motor Setup */
 
 void setup() {
   Serial.begin(9600);
@@ -22,35 +28,42 @@ void setup() {
 
   tft.init(240, 240); 
   tft.fillScreen(ST77XX_BLACK);
-}
 
+  myStepper.setSpeed(60);
+}
+/*
+ * 60 rpm
+ * 1 rotation per second
+ * 1 rotation = 200 steps
+ * 1/200th of second 
+ */ 
 void loop() {
     int yValue = analogRead(joystickY);
     int selectValue = digitalRead(joystickSelect);
-    
-    if (yValue > 1000){
+
+    if (yValue > 900){
       motorPosition--;
-      drawText(String(motorPosition), ST77XX_WHITE);
-      delay(50);
-    } else if (yValue < 10){
+      drawText(String(motorPosition));
+      myStepper.step(-1);
+    } else if (yValue < 100){
       motorPosition++;
-      drawText(String(motorPosition), ST77XX_WHITE);
-      delay(50);
+      drawText(String(motorPosition));
+      myStepper.step(1);
     } else if (selectValue == 0){
+      myStepper.step(-motorPosition);
       motorPosition = 0;
-      drawText(String(motorPosition), ST77XX_WHITE);
-      delay(50);
+      drawText(String(motorPosition));
     }
+    
     Serial.println(motorPosition);
 
     
 }
 
-void drawText(String text, uint16_t color) {
-  tft.fillScreen(ST77XX_BLACK);
+void drawText(String text) {
   tft.setCursor(0, 0);
-  tft.setTextColor(color);
-  tft.setTextSize(10);
+  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
+  tft.setTextSize(2);
   tft.setTextWrap(true);
   tft.print(text);
 }
