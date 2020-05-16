ebaad102f8d95d9e497abc6d2ce21291532cfacd
Travis Bumgarner <travis.bumgarner@gmail.com>
2020-05-16 12:38:58 -0400

    Hooked up joystick to screen

--- unified_parts_demo/unified_parts_demo.ino
+++ unified_parts_demo/unified_parts_demo.ino
@@ -1,10 +1,27 @@
+/* Joystick Setup */
 #define joystickY A1
 #define joystickSelect 2
 int motorPosition = 0;
+/* End Joystick Setup */
+
+/* Screen Setup */
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <SPI.h>
+
+#define TFT_CS        10
+#define TFT_RST        9
+#define TFT_DC         8
+
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
 
 void setup() {
   Serial.begin(9600);
   digitalWrite(joystickSelect, HIGH);
+
+  tft.init(240, 240); 
+  tft.fillScreen(ST77XX_BLACK);
 }
 
 void loop() {
@@ -13,13 +30,27 @@ void loop() {
     
     if (yValue > 1000){
       motorPosition--;
+      drawText(String(motorPosition), ST77XX_WHITE);
       delay(50);
     } else if (yValue < 10){
       motorPosition++;
+      drawText(String(motorPosition), ST77XX_WHITE);
       delay(50);
     } else if (selectValue == 0){
       motorPosition = 0;
+      drawText(String(motorPosition), ST77XX_WHITE);
       delay(50);
     }
     Serial.println(motorPosition);
+
+    
+}
+
+void drawText(String text, uint16_t color) {
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(0, 0);
+  tft.setTextColor(color);
+  tft.setTextSize(10);
+  tft.setTextWrap(true);
+  tft.print(text);
 }
