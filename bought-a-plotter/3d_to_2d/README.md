afe89ee6a48887775954c3dfaa335b731514662b
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-06-12 20:00:34 -0600

    going to rewrite 3d to 2d

---
+++ bought-a-plotter/3d_to_2d/README.md
@@ -0,0 +1,39 @@
+# Tutorials
+
+- [Part 1 Twitch Live Stream](https://www.youtube.com/watch?v=867jL2bwx9c)
+- [Part 2 Twitch Live Stream](https://www.youtube.com/watch?v=t-9eQTv8wtY)
+- [Part 3 Twitch Live Stream](https://www.youtube.com/watch?v=QzO6jngdEAM)
+
+
+# Setup
+
+1. Design & Print drawing instrument holder.
+2. Install drawing instrument holder.
+3. Copy `config.py.example` to `config.py`.
+4. Add some text add the bottom of `main.py` to print.
+5. Execute function.
+6. Upload `.gcode` to 3D Printer.
+7. Attach drawing paper to printer bed.
+8. Execute code in 3D printer.
+
+# Todo
+
+- [x] Refactor to not rely on `characters.py`
+- [x] Write to file
+- [x] Support new lines
+- [x] Finish Numbers & letters
+- [x] Cause printer to not start if illegal characters given
+- [x] Design new pen holder to accept multiple sizes
+- [ ] ~Add support for .!~
+- [ ] ~Remove Tabs for output~
+- [ ] ~Flip x values so x_max is a positive number and not 0~
+- [x] Figure travel and draw height
+- [x] test print a space 
+- [x] Refactor
+    - [x] Move config params to config.py
+    - [x] Other cleanup?
+
+# Pipe Dreams todo
+
+- [ ] Bold text
+- [ ] Italic text
---
+++ bought-a-plotter/3d_to_2d/main.py
@@ -0,0 +1,110 @@
+from datetime import datetime
+import os
+
+from primatives import line_a, line_b, line_c, line_d, line_e, line_f, line_g
+from config import CONFIG
+
+character_map = {
+    '1': 'bc',
+    '2': 'abged',
+    '3': 'abgcd',
+    '4': 'fgbc',
+    '5': 'afgcd',
+    '6': 'afgedc',
+    '7': 'abc',
+    '8': 'abcdefg',
+    '9': 'abcdfg',
+    '0': 'abcdef',
+    'a': 'abgedc',
+    'b': 'fegcd',
+    'c': 'afed',
+    'd': 'gedcb',
+    'e': 'afged',
+    'f': 'afge',
+    'g': 'afedc',
+    'h': 'fgec',
+    'i': 'ea',
+    'j': 'bcde',
+    'k': 'afegc',
+    'l': 'fed',
+    'm': 'aec',
+    'n': 'feabc',
+    'o': 'abcdef',
+    'p': 'abgfe',
+    'q': 'afbgc',
+    'r': 'bafe',
+    's': 'afgcd',
+    't': 'fegd',
+    'u': 'fedcb',
+    'v': 'fbcd',
+    'w': 'fbd',
+    'x': 'fgbec',
+    'y': 'fgbcd',
+    'z': 'abgd',
+    '-': 'g',
+    '_': 'd',
+    ' ': ''
+}
+
+primative_map = {
+    'a': line_a,
+    'b': line_b,
+    'c': line_c,
+    'd': line_d,
+    'e': line_e,
+    'f': line_f,
+    'g': line_g
+}
+
+
+def draw_character(character_to_draw, x_start, y_start):
+    output = f';{character_to_draw}'
+    primatives = character_map[character_to_draw] 
+    for primative in primatives: 
+        draw_primative = primative_map[primative]
+        output += draw_primative(x_start, y_start)
+    return output
+
+def validate_input(sanitized_input):
+    input_character_set = set(sanitized_input)
+    valid_character_set = set(character_map.keys())
+    if not input_character_set.issubset(valid_character_set):
+        invalid_chararacters = input_character_set.difference(valid_character_set)
+        raise Exception(f"Invalid character(s) supplied: {(', ').join(invalid_chararacters)}") 
+
+def main(input_string):
+    sanitized_input = [char.lower() for char in input_string]    
+    validate_input(sanitized_input)
+
+    x_start = CONFIG["X_MIN"]
+    y_start = CONFIG["Y_MIN"]
+    x_period = (CONFIG["X_SPACING"] + CONFIG["CHARACTER_WIDTH"])
+    y_period = (CONFIG["Y_SPACING"] + CONFIG["CHARACTER_HEIGHT"])
+
+    output = ''
+
+    output += 'G28\n'
+    output += f'G0 Z{CONFIG["TRAVEL_HEIGHT"] + 20}\n' # Tick pen on!
+
+    output += f'G0 Z{CONFIG["TRAVEL_HEIGHT"]}\n'
+    for character_to_draw in sanitized_input:
+        if(x_start - x_period) < CONFIG["X_MAX"]:
+            x_start = CONFIG["X_MIN"]
+            y_start += y_period
+
+        if y_start > CONFIG["Y_MAX"]:
+            raise Exception('Exceeded printing bed')
+        
+        output += draw_character(character_to_draw, x_start, y_start)
+        output += '\n'
+        x_start = x_start - x_period
+    output += 'G28\n'
+    output += f'G0 Z{CONFIG["TRAVEL_HEIGHT"] + 20}\n' # Tick pen off!
+    
+    filename = f'{datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.gcode'
+    filepath = os.path.join(os.path.abspath(""), "output") 
+    if not os.path.exists(filepath):
+        os.makedirs(filepath)
+    with open(os.path.join(filepath, filename), 'w') as f:
+        f.write(output)
+main('abcdef012345_-')
