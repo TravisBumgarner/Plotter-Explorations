b53897a1e4e9b99bbfe8c416f7be63fd5240d4f7
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-06-13 19:47:12 -0600

    Added node

--- bought-a-plotter/3_stream_instructions_node/src/stream.ts
+++ bought-a-plotter/3_stream_instructions_node/src/stream.ts
@@ -1,48 +1,61 @@
-/**
- * Instructed ChatGPT to rewrite stream.py in Node.
- */
-
 import { SerialPort } from 'serialport';
 import { ReadlineParser } from '@serialport/parser-readline';
 import fs from 'fs';
 
-// Open grbl serial port
-const port = new SerialPort({ path: '/dev/cu.usbserial-10', baudRate: 115200 });
-const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
-port.pipe(parser);
-
-// Open g-code file
-const gcodeFilePath = 'grbl.gcode';
-const gcodeFileContent = fs.readFileSync(gcodeFilePath, 'utf-8');
-const gcodeLines = gcodeFileContent.split('\n');
-
-// Wake up grbl
-port.write('\r\n\r\n');
-setTimeout(() => {
-  port.flush();
-  console.log('Grbl initialized');
-  streamGcode();
-}, 2000);
-
-// Stream g-code to grbl
-function streamGcode() {
-  for (const line of gcodeLines) {
-    const l = line.trim(); // Strip all EOL characters for consistency
-    console.log(`Sending: ${l}`);
-    port.write(`${l}\n`);
-  }
+async function sendInstructionAndWaitForReply(instruction: string, port: SerialPort): Promise<string> {
+  return new Promise<string>((resolve) => {
+    const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+    parser.once('data', (line) => {
+      const grbl_out = line.trim();
+      resolve(grbl_out);
+    });
+    port.write(instruction + '\n');
+  });
 }
 
-// Handle grbl response
-parser.on('data', (line) => {
-  const grbl_out = line.trim();
-  console.log(`Received: ${grbl_out}`);
-});
+async function streamGcode(gcodeFilePath: string, path: string, baudRate: number): Promise<void> {
+  // Open grbl serial port
+  const port = new SerialPort({path, baudRate });
 
-// Wait until grbl is finished to close serial port and file
-console.log('Press <Enter> to exit and disable grbl.');
-process.stdin.once('data', () => {
+  // Open g-code file
+  const gcodeFileContent = fs.readFileSync(gcodeFilePath, 'utf-8');
+  const gcodeLines = gcodeFileContent.split('\n');
+
+  // Wake up grbl
+  await new Promise<void>((resolve) => {
+    port.write('\r\n\r\n');
+    resolve()
+  });
+
+  // Wait for grbl to initialize
+  await new Promise<void>((resolve) => setTimeout(resolve, 2000));
+
+  // Flush startup text in serial input
+  await new Promise<void>((resolve) => {
+    port.flush();
+    resolve()
+  });
+
+  console.log('Grbl initialized');
+
+  // Stream g-code to grbl
+  for (const line of gcodeLines) {
+    const instruction = line.trim(); // Strip all EOL characters for consistency
+    console.log(`Sending: ${instruction}`);
+
+    const reply = await sendInstructionAndWaitForReply(instruction, port);
+    console.log(`Received: ${reply}`);
+  }
+
+  // Close the serial port
   port.close();
-});
 
-export {}
+  console.log('All instructions sent and replies received.');
+  process.exit(0)
+}
+
+// Usage example:
+streamGcode('grbl.gcode', '/dev/cu.usbserial-10', 115200)
+  .catch((error) => {
+    console.error('An error occurred:', error);
+  });
