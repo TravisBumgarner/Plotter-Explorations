72c9afe198a6eb422da15388e74a5df78c41a424
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-06-18 18:38:44 -0600

    added button presses woo

--- bought-a-plotter/5_chat_and_plot/frontend/src/index.tsx
+++ bought-a-plotter/5_chat_and_plot/frontend/src/index.tsx
@@ -1,18 +1,18 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { w3cwebsocket as W3CWebSocket } from "websocket"
-import styled from 'styled-components'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
+import styled from 'styled-components';
 
-const client = new W3CWebSocket('ws://127.0.0.1:5001')
+const client = new W3CWebSocket('ws://127.0.0.1:5001');
 
 const AppWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100vw;
-    height: 100vh;
-    flex-direction: column;
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  flex-direction: column;
+`;
 
 const ChatClientWrapper = styled.div`
     width: 70vw;
@@ -21,105 +21,160 @@ const ChatClientWrapper = styled.div`
     padding 10px;
     display: flex;
     flex-direction: column;
-`
+`;
 
 const ChatInputWrapper = styled.div`
-    display: flex;
-    min-height: 30px;
-`
+  display: flex;
+  min-height: 30px;
+`;
 
 const ChatInput = styled.input`
-    // box-sizing: border-box;
-    flex-grow: 1;
-    flex: 1;
-`
+  // box-sizing: border-box;
+  flex-grow: 1;
+  flex: 1;
+`;
 
-const ChatInputSubmit = styled.button``
-
-const ChatMessage = styled.div`
-`
+const ChatMessage = styled.div``;
 
 const ChatMessagesWrapper = styled.div`
-    width: 100%;
-    flex-grow: 1;
-    background-color: rgba(0,0,0,0.2);
-    overflow: scroll;
-`
-
-
-const makeFakeUser = () => 'User' + (Math.random() * 1000000 + '').split('.')[0]
-const user = makeFakeUser()
+  width: 100%;
+  flex-grow: 1;
+  background-color: rgba(0, 0, 0, 0.2);
+  overflow: scroll;
+`;
 
 const App = () => {
-    const [hasConnected, setHasConnected] = React.useState<boolean>(false)
-    const [messages, setMessages] = React.useState<{sender: string, message: string}[]>([])
-    const [content, setContent] = React.useState('')
+  const [hasConnected, setHasConnected] = React.useState<boolean>(false);
+  const [messages, setMessages] = React.useState<{ sender: string; message: string }[]>([]);
+  const [content, setContent] = React.useState('');
 
-    client.onmessage = (message) => {
-        console.log(message.data)
-        setMessages([...messages, {sender: "PlotterGPT", message: message.data}])
-    }
-    if (!hasConnected) {
-        client.onopen = (message) => {
-            setHasConnected(true)
-        }
-        return <p>Loading</p>
-    }
+  const sendMessage = (content: string) => {
+    const encodedMessage = JSON.stringify({
+      content,
+      sender: 'User',
+      type: 'TRANSMIT_MESSAGE',
+    });
+    client.send(encodedMessage);
+    setMessages([{ sender: 'User', message: content }, ...messages]);
+  };
 
+  const moveLeft = () => {
+    sendMessage('G91\r\nG0 X-10\r\nG90');
+  };
 
-    const chatMessages = messages.map(({message, sender}, index) => {
-        return <ChatMessage key={index}><strong>{sender}</strong>: {message}</ChatMessage>
-    })
+  const moveRight = () => {
+    sendMessage('G91\r\nG0 X10\r\nG90');
+  };
 
-    const submit = () => {
-        sendMessage(content)
-        setContent('')
-    }
+  const moveUp = () => {
+    sendMessage('G91\r\nG0 Y10\r\nG90');
+  };
 
-    const sendMessage = (content: string) => {
-        const encodedMessage = JSON.stringify({
-            content,
-            sender: user,
-            type: 'TRANSMIT_MESSAGE'
-        })
-        client.send(encodedMessage)
-        setMessages([...messages, {sender: "User", message: content}])
-    } 
+  const moveDown = () => {
+    sendMessage('G91\r\nG0 Y-10\r\nG90');
+  };
 
-    const handleKeyPress = (event) => {
-        if (event.key === 'Enter') {
-          submit();
-        }
-      };
-
-    return (
-        <AppWrapper>
-            <ChatClientWrapper>
-                <h1>Hello {user}</h1>
-                <ChatMessagesWrapper>
-                    {chatMessages}
-                </ChatMessagesWrapper>
-                <ChatInputWrapper>
-                    <ChatInput onKeyPress={handleKeyPress} value={content} onChange={(event) => setContent(event.target.value.toUpperCase())}/>
-                </ChatInputWrapper>
-            </ChatClientWrapper>
-            <ButtonsWrapper>
-                <button onClick={() => sendMessage('M3 S0')}>Pen Up</button>
-                <button onClick={() => sendMessage('M3 S1000')}>Pen Down</button>
-            </ButtonsWrapper>
-        </AppWrapper>
-    )
+  const handleArrowKeyPress = (event) => {
+    console.log('RUDA', event)
+    switch (event.key) {
+      case 'Enter':
+        submit();
+        break;
+      case 'ArrowLeft':
+        moveLeft();
+        break;
+      case 'ArrowRight':
+        moveRight();
+        break;
+      case 'ArrowUp':
+        moveUp();
+        break;
+      case 'ArrowDown':
+        moveDown();
+        break;
+      default:
+        break;
+  };
 }
 
+  React.useEffect(() => {
+    console.log('hi')
+    // Attach event listeners for arrow key presses
+    document.addEventListener('keydown', handleArrowKeyPress, true);
+
+    // Clean up the event listeners when the component is unmounted
+    // return () => {
+    //   document.removeEventListener('keypress', handleArrowKeyPress);
+    // };
+  }, []);
+
+  client.onmessage = (message) => {
+    console.log(message.data);
+    setMessages([{ sender: 'PlotterGPT', message: message.data }, ...messages]);
+  };
+  if (!hasConnected) {
+    client.onopen = (message) => {
+      setHasConnected(true);
+    };
+    return <p>Loading</p>;
+  }
+
+  const chatMessages = messages.map(({ message, sender }, index) => {
+    return (
+      <ChatMessage key={index}>
+        <strong>{sender}</strong>: {message}
+      </ChatMessage>
+    );
+  });
+
+  const submit = () => {
+    sendMessage(content);
+    setContent('');
+  };
+
+
+
+  const handleChatKeyPress = (event) => {
+    console.log('chat key', event.key)
+    switch (event.key) {
+      case 'Enter':
+        submit();
+    }
+  };
+
+  return (
+    <AppWrapper>
+      <ChatClientWrapper>
+        <ChatMessagesWrapper>{chatMessages}</ChatMessagesWrapper>
+        <ChatInputWrapper>
+          <ChatInput
+            onKeyPress={handleChatKeyPress}
+            value={content}
+            onChange={(event) => setContent(event.target.value.toUpperCase())}
+          />
+        </ChatInputWrapper>
+      </ChatClientWrapper>
+      <ButtonsWrapper>
+        <button onClick={() => sendMessage('G28')}>Go Home</button>
+        <button onClick={() => sendMessage('M3 S0')}>Pen Up</button>
+        <button onClick={() => sendMessage('M3 S1000')}>Pen Down</button>
+        <button onClick={() => sendMessage('$2=3')}>Invert X and Y</button>
+        <button onClick={() => sendMessage('G92 X0 Y0')}>Set Home</button>
+        <button onClick={() => sendMessage('G91\r\nG0 X-10\r\nG90')}>Move Left</button>
+        <button onClick={() => sendMessage('G91\r\nG0 X10\r\nG90')}>Move Right</button>
+        <button onClick={() => sendMessage('G91\r\nG0 Y10\r\nG90')}>Move Up</button>
+        <button onClick={() => sendMessage('G91\r\nG0 Y-10\r\nG90')}>Move Down</button>
+      </ButtonsWrapper>
+    </AppWrapper>
+  );
+};
+
 const ButtonsWrapper = styled.div`
-    display: flex; 
-    flex-direction: row;
-`
+  display: flex;
+  flex-direction: row;
+`;
 
-export default App
-export { client }
+export default App;
+export { client };
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'));
