3ba9eac5d456e693aafbd99edd040cddbef7d847
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-06-23 23:28:18 -0600

    algorithm progress

--- bought-a-plotter/10_edge_detection/main.py
+++ bought-a-plotter/10_edge_detection/main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
 
+X_MARKS_THE_SPOT = 'X'
+
 def img_to_black_and_white(filename):
     img = cv2.imread(filename)
     print(img)
@@ -12,12 +14,12 @@ def img_to_black_and_white(filename):
 
 
 
-def find_edges(img):
+def find_point(img):
     # this is square, these might accidentally be flipped. 
     [width,height] = img.shape
 
     # Create a place where edges will be populated.
-    edges = np.array([["0" for i in range(0,width)] for j in range(0,height)])
+    edges = np.array([[" " for i in range(0,width)] for j in range(0,height)])
 
     for y_pos, row in enumerate(img):
         for x_pos, pixel in enumerate(row):
@@ -32,9 +34,9 @@ def find_edges(img):
             # For now, we'll just grab whichever position is value of 1 and save that in our edges array.
             
             if pixel == 1:
-                edges[y_pos, x_pos] = 1
+                edges[y_pos, x_pos] = X_MARKS_THE_SPOT
             else:
-                edges[y_pos, x_pos + 1] = 1
+                edges[y_pos, x_pos + 1] = X_MARKS_THE_SPOT
 
     # Copying the code above, and transposing edges and image. Because I'm lazy.
 
@@ -53,13 +55,64 @@ def find_edges(img):
             # For now, we'll just grab whichever position is value of 1 and save that in our edges array.
             
             if pixel == 1:
-                edges[y_pos, x_pos] = 1
+                edges[y_pos, x_pos] = X_MARKS_THE_SPOT
             else:
-                edges[y_pos, x_pos + 1] = 1
-
-    return np.transpose(edges)
-
+                edges[y_pos, x_pos + 1] = X_MARKS_THE_SPOT
     
+    edges = np.transpose(edges)
+    for row in edges:
+        print(row)
+
+    output = []
+    for y_pos, row in enumerate(edges):
+        for x_pos, pixel in enumerate(row):
+            if pixel == X_MARKS_THE_SPOT:
+                output.append((x_pos,y_pos))
+    return output
+
+class LinkedListItem:
+    def __init__(self, value):
+        self.value = value
+        self.next = []
+
+    def add_next(self, value):
+        self.next.append(value)
+
+    def get_next(self):
+        return self.next
+
+def make_window(x,y):
+    return [
+        (x-1, y-1),
+        (x-1, y),
+        (x-1, y+1),
+        (x, y-1),
+        (x, y+1),
+        (x+1, y-1),
+        (x+1, y),
+        (x+1, y+1)
+    ]
+
+def find_path(points):
+    remaining_points = points.copy()
+    
+    current_point = LinkedListItem(points[0])
+    points.remove(points[0])
+
+    [x,y] = current_point.value
+    window = make_window(x,y)
+
+    for point in window:
+        if point in remaining_points:
+            remaining_points.remove(point)
+            current_point.add_next(LinkedListItem(point))
+
+    # I need to make this recursive -_-
+
+    print(current_point.value)
+    print([item.value for item in current_point.get_next()])
+
+    return
 
 
 
@@ -68,9 +121,12 @@ def main():
     print("output to bnw\n", black_and_white)
     # print("shape", black_and_white.shape)
 
-    edges = find_edges(black_and_white)
-    for row in edges:
+    points = find_point(black_and_white)
+    for row in points:
         print(row)
 
+    # Eventually we'll need a find_paths function. 
+    path = find_path(points)
+    print(path)
 
 main()
