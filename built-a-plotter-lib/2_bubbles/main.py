3627643f70490b5cb099955a0b6bc06a0f10c014
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-11-28 19:39:30 -0700

    implemented

--- built-a-plotter-lib/2_bubbles/main.py
+++ built-a-plotter-lib/2_bubbles/main.py
@@ -4,11 +4,12 @@ from random import randrange, choice, randint
 LINE_WIDTH = 1.0
 
 COLORS = [
-    {"title": "purple1", "color": "#EABEFF"},
-    {"title": "blue1", "color": "#A2FFF8"},
-    {"title": "orange1", "color": "#FF7700"},
-    {"title": "purple2", "color": "#AD00FF"},
-    {"title": "grey1", "color": "#E9E9E9"},
+    {"title": "4_green1", "color": "#A9FF00"},
+    {"title": "6_blue1", "color": "#A2FFF8"},
+    {"title": "7_blue2", "color": "#0024FF"},
+    {"title": "c_purple2", "color": "#AD00FF"},
+    {"title": "a_pink2", "color": "#FF0096"},
+    {"title": "f_grey3", "color": "#1E1E1E"},
 ]
 
 # Create a plotter object
@@ -30,9 +31,31 @@ for color in COLORS:
 
 print
 counter = 0
-STARTING_CIRCLES = 500
+STARTING_CIRCLES = 100
+
+center_points = []
+
+
+def is_near_existing_center_point(x, y):
+    for center_point in center_points:
+        if abs(center_point[0] - x) < 15 and abs(center_point[1] - y) < 15:
+            return True
+    return False
+
 
 while counter < STARTING_CIRCLES:
+    attempt_to_find_better_center = 0
+
+    while attempt_to_find_better_center < 100000:
+        x_center = randrange(int(plotter.x_min), int(plotter.x_max))
+        y_center = randrange(int(plotter.y_min), int(plotter.y_max))
+
+        if not is_near_existing_center_point(x_center, y_center):
+            break
+        attempt_to_find_better_center += 1
+    print("attempt_to_find_better_center", attempt_to_find_better_center)
+
+    center_points.append([x_center, y_center])
     x_center = randrange(int(plotter.x_min), int(plotter.x_max))
     y_center = randrange(int(plotter.y_min), int(plotter.y_max))
 
@@ -43,17 +66,18 @@ while counter < STARTING_CIRCLES:
         abs(plotter.y_max - y_center),
     )
 
-    remaining_radius = min(randint(0, 20), min_distance_to_edge)
+    remaining_radius = min(randint(0, 10), min_distance_to_edge)
 
+    first_circle = True
     while remaining_radius > 0:
-        color = choice(COLORS)
-        layer = color["title"]
+        layer = "f_grey3" if first_circle else choice(COLORS)["title"]
         plotter.layers[layer].add_circle(
             x_center,
             y_center,
             radius=remaining_radius,
         )
-        remaining_radius -= randint(int(LINE_WIDTH), int(remaining_radius))
+        remaining_radius -= randint(int(LINE_WIDTH), int(LINE_WIDTH * 5))
+        first_circle = False
     counter += 1
 
 
--- built-a-plotter-lib/4_josef_albers_many/main.py
+++ built-a-plotter-lib/4_josef_albers_many/main.py
@@ -1,17 +1,14 @@
 from gcode2dplotterart import Plotter2D
-from random import randrange, shuffle, random
+from random import randrange, shuffle
 import math
-from typing import Dict, List, Union
-import time
 
-print("hi")
+
 LINE_WIDTH = 2.5
 
 COLORS = [
     {"title": "color1", "color": "#A2FFF8"},
     {"title": "color2", "color": "#FF0096"},
     {"title": "color3", "color": "#AD00FF"},
-    {"title": "DONT PLOT", "color": "#FFFFFF"},
     {"title": "color4", "color": "#E9E9E9"},
 ]
 
@@ -24,6 +21,13 @@ plotter = Plotter2D(
     feed_rate=10000,
 )
 
+DONT_PLOT_LAYER = {
+    "title": "DONT PLOT",
+    "color": "#FFFFFF",
+    "line_width": LINE_WIDTH,
+}
+plotter.add_layer(**DONT_PLOT_LAYER)
+
 for color in COLORS:
     plotter.add_layer(
         title=color["title"],
@@ -31,9 +35,18 @@ for color in COLORS:
         line_width=LINE_WIDTH,
     )
 
+# Should be less than 100 so that the last square can be one later on.
+SIDE_LENGTH_PERCENTAGE_CHOICES = [i / 100 for i in range(10, 99, 20)]
+
 
 def josef_albers(x_min: float, y_min: float, side_length: float):
-    shuffle(COLORS)
+    colors = COLORS.copy()
+
+    # Append some number of white layers.
+    for i in range(randrange(0, 3)):
+        colors.append(DONT_PLOT_LAYER)
+
+    shuffle(colors)
 
     side_padding = int(side_length * 0.2)
     x_center = x_min + side_length / 2
@@ -43,44 +56,26 @@ def josef_albers(x_min: float, y_min: float, side_length: float):
 
     vertical_angle = math.degrees(math.atan(int(side_length / 2) / (y_center - y_min)))
 
-    square_side_length_percentages = sorted([random() for i in range(len(COLORS))])
-    print(square_side_length_percentages)
-    # square_side_length_percentages = [0.2, 0.4, 0.6, 0.8, 1]
-    # square_side_length_percentages = [0.2, 0.4, 0.6, 0.8, 1]
+    shuffle(SIDE_LENGTH_PERCENTAGE_CHOICES)
+    square_side_length_percentages = SIDE_LENGTH_PERCENTAGE_CHOICES[: len(colors) - 1]
+    square_side_length_percentages.append(1)
 
-    square_side_lengths = [
-        int(side_length * percentage) for percentage in square_side_length_percentages
-    ]
-    sorted(square_side_lengths)
-    print(square_side_lengths)
+    square_side_lengths = sorted(
+        [int(side_length * percentage) for percentage in square_side_length_percentages]
+    )
 
     current_side_length = LINE_WIDTH
-    for index, color in enumerate(COLORS):
+    for index, color in enumerate(colors):
         threshold_side_length = square_side_lengths[index]
 
         while current_side_length < threshold_side_length:
             x_left_of_center = current_side_length / 2
-            print("xleft", x_left_of_center)
             y_below_center = x_left_of_center / math.tan(math.radians(vertical_angle))
-            print("ybelow", y_below_center)
             x_start = x_center - x_left_of_center
             y_start = y_center - y_below_center
 
             x_end = x_start + current_side_length
             y_end = y_start + current_side_length
-
-            print(
-                "side_length",
-                current_side_length,
-                "x_start",
-                x_start,
-                "y_start",
-                y_start,
-                "x_end",
-                x_end,
-                "y_end",
-                y_end,
-            )
 
             plotter.layers[color["title"]].add_rectangle(
                 x_start=x_start,
@@ -91,11 +86,8 @@ def josef_albers(x_min: float, y_min: float, side_length: float):
 
             current_side_length += LINE_WIDTH
 
-        # Give a little bit of spacing between color layers.
-        current_side_length += LINE_WIDTH / 2
 
-
-SIDE_LENGTH = 25
+SIDE_LENGTH = 50
 
 for x in range(0, plotter.width - SIDE_LENGTH, SIDE_LENGTH + 5):
     for y in range(0, plotter.height - SIDE_LENGTH, SIDE_LENGTH + 5):
