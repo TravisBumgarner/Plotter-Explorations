a40bfd8ac3c7667f00e8ed99c285bb3a03ae969d
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-11-28 17:32:40 -0700

    removed bad code

--- built-a-plotter-lib/3_josef_albers/main.py
+++ built-a-plotter-lib/3_josef_albers/main.py
@@ -1,28 +1,18 @@
 from gcode2dplotterart import Plotter2D
 from random import randrange, shuffle
 import math
+from typing import Dict, List, Union
+import time
 
-LINE_WIDTH = 1.0
+LINE_WIDTH = 2.5
 
 COLORS = [
-    {"title": "1_red1", "color": "#FF4141"},
-    {"title": "2_orange1", "color": "#FF7700"},
-    {"title": "3_yellow1", "color": "#FFDB11"},
-    {"title": "4_green1", "color": "#A9FF00"},
-    {"title": "5_green2", "color": "#00E350"},
     {"title": "6_blue1", "color": "#A2FFF8"},
-    {"title": "7_blue2", "color": "#0024FF"},
-    {"title": "8_blue3", "color": "#5D9DB4"},
-    {"title": "9_pink1", "color": "#FF91D2"},
     {"title": "a_pink2", "color": "#FF0096"},
-    {"title": "b_purple1", "color": "#EABEFF"},
     {"title": "c_purple2", "color": "#AD00FF"},
     {"title": "d_grey1", "color": "#E9E9E9"},
-    {"title": "e_grey2", "color": "#B1B1B1"},
-    {"title": "f_grey3", "color": "#1E1E1E"},
 ]
 
-# Create a plotter object
 plotter = Plotter2D(
     title="Josef Albers Homage",
     x_min=0,
@@ -42,28 +32,44 @@ for color in color_choices:
         line_width=LINE_WIDTH,
     )
 
+SIDE_PADDING = int(plotter.width * 0.2)
+
 x_center = (plotter.x_max - plotter.x_min) / 2
-y_center = randrange(int(plotter.y_min), int(plotter.y_max))
+y_center = randrange(
+    int(plotter.y_min) + SIDE_PADDING, int(plotter.y_max) - SIDE_PADDING
+)
 
 vertical_angle = math.degrees(math.atan(int(plotter.width / 2) / (y_center)))
 
-for color in color_choices:
-    box_side_length = randrange(1, int(plotter.width))
+square_side_length_percentages = [0.4, 0.7, 0.9, 1]
 
-    x_left_of_center = box_side_length / 2
-    y_below_center = x_left_of_center / math.tan(math.radians(vertical_angle))
+square_side_lengths = [
+    int(plotter.width * percentage) for percentage in square_side_length_percentages
+]
+sorted(square_side_lengths)
 
-    x_start = x_center - x_left_of_center
-    y_start = y_center - y_below_center
+current_side_length = LINE_WIDTH
+for index, color in enumerate(color_choices):
+    threshold_side_length = square_side_lengths[index]
 
-    x_end = x_start + box_side_length
-    y_end = y_start + box_side_length
+    while current_side_length < threshold_side_length:
+        x_left_of_center = current_side_length / 2
+        y_below_center = x_left_of_center / math.tan(math.radians(vertical_angle))
 
-    plotter.layers[color["title"]].add_rectangle(
-        x_start=x_start, y_start=y_start, x_end=x_end, y_end=y_end
-    )
+        x_start = x_center - x_left_of_center
+        y_start = y_center - y_below_center
 
+        x_end = x_start + current_side_length
+        y_end = y_start + current_side_length
+
+        plotter.layers[color["title"]].add_rectangle(
+            x_start=x_start,
+            y_start=y_start,
+            x_end=x_end,
+            y_end=y_end,
+        )
+
+        current_side_length += LINE_WIDTH
 
 plotter.preview()
-
 plotter.save()
