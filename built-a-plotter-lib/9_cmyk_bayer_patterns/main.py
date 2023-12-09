17265a591cf53a2fdc65202af9e63d955c6d9266
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-12-09 13:34:59 -0700

    refactored to points

--- built-a-plotter-lib/9_cmyk_bayer_patterns/main.py
+++ built-a-plotter-lib/9_cmyk_bayer_patterns/main.py
@@ -1,7 +1,7 @@
 from gcode2dplotterart import Plotter2D
 import cv2
 from typing import List, Tuple
-import math
+from random import shuffle
 
 # TODO - make a dictionary of primaries to other colors.
 
@@ -22,12 +22,36 @@ MAGENTA_LAYER = "magenta"
 YELLOW_LAYER = "yellow"
 BLACK_LAYER = "black"
 
-color_lookup = {
-    (0, 255, 255): CYAN_LAYER,
-    (255, 0, 255): MAGENTA_LAYER,
-    (255, 255, 0): YELLOW_LAYER,
-    (0, 0, 0): BLACK_LAYER,
+
+# Ideally percentages are divisible by 25, so that the number of points can be divided into quarters.
+color_name_to_cyan_percentage = {
+    "red": {"cyan": 0, "magenta": 50, "yellow": 50, "black": 0},
+    "orange": {"cyan": 0, "magenta": 50, "yellow": 50, "black": 0},
+    "yellow": {"cyan": 0, "magenta": 0, "yellow": 100, "black": 0},
+    "green": {"cyan": 50, "magenta": 0, "yellow": 50, "black": 0},
+    "blue": {"cyan": 50, "magenta": 50, "yellow": 0, "black": 0},
+    "cyan": {"cyan": 100, "magenta": 0, "yellow": 0, "black": 0},
+    "magenta": {"cyan": 0, "magenta": 100, "yellow": 0, "black": 0},
+    "black": {"cyan": 0, "magenta": 0, "yellow": 0, "black": 100},
+    "white": {"cyan": 0, "magenta": 0, "yellow": 0, "black": 0},
 }
+
+rgb_to_color_name = {
+    (255, 0, 0): "red",
+    (255, 165, 0): "orange",
+    (255, 255, 0): "yellow",
+    (0, 255, 0): "green",
+    (0, 0, 255): "blue",
+    (0, 255, 255): "cyan",
+    (255, 0, 255): "magenta",
+    (0, 0, 0): "black",
+    (255, 255, 255): "white",
+}
+
+if set(color_name_to_cyan_percentage.keys()) != set(rgb_to_color_name.values()):
+    raise ValueError(
+        "The color names in color_name_to_cyan_percentage must match the color names in rgb_to_color_name."
+    )
 
 LAYERS = [CYAN_LAYER, MAGENTA_LAYER, YELLOW_LAYER, BLACK_LAYER]
 
@@ -39,7 +63,7 @@ def find_nearest_color(rgb_value: Tuple[int, int, int]):
     # Calculate the Euclidean distance to find the nearest color
     distances = {
         color: sum((a - b) ** 2 for a, b in zip(rgb_value, color))
-        for color in color_lookup
+        for color in rgb_to_color_name
     }
 
     nearest_color = min(distances, key=distances.get)
@@ -47,10 +71,10 @@ def find_nearest_color(rgb_value: Tuple[int, int, int]):
     return nearest_color
 
 
-def map_color(sample_square, sample_size):
-    if sample_square.shape != (sample_size, sample_size, 3):
+def map_color(sample_square, pixels_per_sample_side):
+    if sample_square.shape != (pixels_per_sample_side, pixels_per_sample_side, 3):
         raise ValueError(
-            f"Input sample must be a {sample_size}x{sample_size} square with 3 color channels."
+            f"Input sample must be a {pixels_per_sample_side}x{pixels_per_sample_side} square with 3 color channels."
         )
     pixel_values = [element for row in sample_square for element in row]
 
@@ -60,9 +84,9 @@ def map_color(sample_square, sample_size):
 
     dominant_color = max(color_counts, key=color_counts.get)
 
-    layer = color_lookup.get(dominant_color)
+    color_name = rgb_to_color_name.get(dominant_color)
 
-    return layer
+    return color_name
 
 
 def read_and_prep_image(
@@ -85,47 +109,91 @@ def read_and_prep_image(
     return img
 
 
-def sample_img(img: List[List[Tuple[int, int, int]]], sample_size: int):
+def image_to_cmyk_color_ratios(
+    img: List[List[Tuple[int, int, int]]], pixels_per_sample_side: int
+):
     """
     Take in an image, and return a 2D list of colors to plot as output.
 
     Params:
       img: The image to process
-      sample_size: the number of pixels, along a side to sample. The number of pixels would then be sample_size**2.
+      pixels_per_sample_side: the number of pixels, along a side to sample. The number of pixels would then be pixels_per_sample_side**2.
     """
 
     output = []
 
-    for starting_row in range(0, len(img), sample_size):
+    for starting_row in range(0, len(img), pixels_per_sample_side):
         output_row = []
-        for starting_col in range(0, len(img[0]), sample_size):
+        for starting_col in range(0, len(img[0]), pixels_per_sample_side):
             img_section = img[
-                starting_row : starting_row + sample_size,
-                starting_col : starting_col + sample_size,
+                starting_row : starting_row + pixels_per_sample_side,
+                starting_col : starting_col + pixels_per_sample_side,
             ]
-            result = map_color(img_section, sample_size)
-            output_row.append(result)
+            result = map_color(img_section, pixels_per_sample_side)
+            cmyk_ratio = color_name_to_cyan_percentage.get(result)
+            output_row.append(cmyk_ratio)
         output.append(output_row)
 
     return output
 
 
+def plot_points_per_cmyk_ratio(
+    cmyk_ratio,
+    x_start_mm,
+    y_start_mm,
+    mm_per_section_side,
+    points_per_sample_side,
+):
+    points = []
+    for color, percentage in cmyk_ratio.items():
+        num_points = int(percentage / 100 * points_per_sample_side**2)
+        for i in range(num_points):
+            points.append(color)
+
+    shuffle(points)
+
+    x_spacing = mm_per_section_side / points_per_sample_side
+    y_spacing = mm_per_section_side / points_per_sample_side
+
+    for i in range(points_per_sample_side):
+        for j in range(points_per_sample_side):
+            x = x_start_mm + i * x_spacing
+            y = y_start_mm - j * y_spacing
+            plot_color = points.pop()
+
+            plotter.layers[plot_color].add_point(
+                x=x,
+                y=y,
+            )
+
+
 def main():
-    circle_diameter = 10
+    mm_per_section_side = 10
+    points_per_sample_side = 2
+    pixels_per_sample_side = 100
+    section_padding = 1.2
 
-    img = read_and_prep_image("./squares.png")
-    sampled_img = sample_img(img, sample_size=1)
+    img = read_and_prep_image("./test.png")
+    cmyk_color_ratios = image_to_cmyk_color_ratios(
+        img, pixels_per_sample_side=pixels_per_sample_side
+    )
 
-    for row_index, row in enumerate(sampled_img):
-        for col_index, color in enumerate(row):
-            total_rows = len(sampled_img)
-            x_center = col_index * circle_diameter + circle_diameter
-            y_center = (total_rows - row_index) * circle_diameter + circle_diameter
+    for row_index, row in enumerate(cmyk_color_ratios):
+        for col_index, cmyk_ratio in enumerate(row):
+            total_rows = len(cmyk_color_ratios)
+            x_start_mm = (
+                col_index * mm_per_section_side * section_padding + mm_per_section_side
+            )
+            y_start_mm = (
+                total_rows - row_index
+            ) * mm_per_section_side * section_padding + mm_per_section_side
 
-            plotter.layers[color].add_circle(
-                x_center=x_center,
-                y_center=y_center,
-                radius=circle_diameter / 2 - circle_diameter * 0.15,
+            plot_points_per_cmyk_ratio(
+                cmyk_ratio=cmyk_ratio,
+                x_start_mm=x_start_mm,
+                y_start_mm=y_start_mm,
+                mm_per_section_side=mm_per_section_side,
+                points_per_sample_side=points_per_sample_side,
             )
     plotter.preview()
 
